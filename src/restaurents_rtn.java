5dde853ae3cb5ed946d874adbc3b898aa244ce66
Sutar <nikhilsutar621@gmail.com>
2024-02-04 22:27:21 +0530

    Enhance GUI and Add View Map Functionality
    Refactor GUI layout and design for improved aesthetics and usability.
    
    Introduce a method to display Google Maps embedded view upon clicking the "View Map" button.
    
    Implement a button to enable users to browse restaurants in Ratnagiri via a map interface.
    
    Update button appearance and functionality for consistency and user feedback.

--- src/restaurents_rtn.java
+++ src/restaurents_rtn.java
@@ -55,20 +55,25 @@ public class restaurents_rtn extends JFrame implements ActionListener {
         view_map.addActionListener(this);
         contentPanel.add(view_map);
 
-        ImageIcon imageIcon1 = new ImageIcon("src/Dash_Icons/browse_restro.png");
-        Image image1 = imageIcon1.getImage().getScaledInstance(845,81, Image.SCALE_DEFAULT);
-        imageIcon1 = new ImageIcon(image1);
-        JLabel label1 = new JLabel(imageIcon1);
-        label1.setBounds(250, 250, 845,81);
-        contentPanel.add(label1);
-
-        places_ratnagiri.loadAndSetCustomFont(label1, "\\tourmate1\\src\\icons\\Poppins-Light.ttf", "Browse Ratnagiri by Food", Font.BOLD, 33, new Color(0xFFFFFF), 280, 13, 531,50);
-
         JLabel label_a = createImageLabel("src/Dash_Icons/browse_restro.png", 845, 81, 250, 250);
         contentPanel.add(label_a);
+        places_ratnagiri.loadAndSetCustomFont(label_a, "\\tourmate1\\src\\icons\\Poppins-Light.ttf", "Browse Ratnagiri by Food", Font.BOLD, 33, new Color(0xFFFFFF), 280, 13, 531,50);
+
 
         JLabel label_b = createImageLabel("src/Dash_Icons/cuisines.png",386,499,12,490);
         contentPanel.add(label_b);
+
+        JLabel label_c = createImageLabel("src/Dash_Icons/Delivery.png",418,91,0,490+20+399+100);
+        contentPanel.add(label_c);
+
+        JButton viewMapButton = createImageButton("src/Dash_Icons/view_on_map.png", 20, 250, 353, 223);
+        viewMapButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                // Action to perform when the button is clicked
+                System.out.println("View Map button clicked!");
+            }
+        });
+        contentPanel.add(viewMapButton);
 
         rtn.setUndecorated(true);
         rtn.setVisible(true);
@@ -93,6 +98,14 @@ public class restaurents_rtn extends JFrame implements ActionListener {
         return label;
     }
 
+    public static JButton createImageButton(String imagePath, int x, int y, int width, int height) {
+        ImageIcon icon = new ImageIcon(imagePath);
+        JButton button = new JButton(icon);
+        button.setBackground(new Color(0xDB9205));
+        button.setBounds(x, y, width, height);
+        button.setBorder(BorderFactory.createEmptyBorder());
+        return button;
+    }
 
     @Override
     public void actionPerformed(ActionEvent e) {
@@ -100,7 +113,7 @@ public class restaurents_rtn extends JFrame implements ActionListener {
         if (e.getSource() == view_map) {
             Platform.runLater(() -> {
                 JFrame frame1 = new JFrame();
-                frame1.setSize(1366,768);
+                frame1.setSize(1366,771);
                 frame1.setExtendedState(MAXIMIZED_BOTH);
                 WebView webView = new WebView();
 
@@ -114,7 +127,7 @@ public class restaurents_rtn extends JFrame implements ActionListener {
                         "</head>\n" +
                         "<body>\n" +
                         "    <iframe\n" +
-                        "        src=\"https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d61050.18280636948!2d73.26917434443865!3d16.99242496001141!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1srestaurants%20in%20ratnagiri%20embed%20on%20map!5e0!3m2!1sen!2sin!4v1706975237726!5m2!1sen!2sin\" width=\"1372\" height=\"772\" style=\"border:0;\" allowfullscreen=\"\" loading=\"lazy\" referrerpolicy=\"no-referrer-when-downgrade\">\n" +
+                        "        src=\"https://www.google.com/maps/embed?pb=!1m12!1m8!1m3!1d61050.185489388576!2d73.26917424414033!3d16.992416720036204!3m2!1i1024!2i768!4f13.1!2m1!1srestaurants%20in%20ratnagiri%20embed%20on%20map!5e0!3m2!1sen!2sin!4v1707014888645!5m2!1sen!2sin\" width=\"1366\" height=\"820\" style=\"border:0;\" allowfullscreen=\"\" loading=\"lazy\" referrerpolicy=\"no-referrer-when-downgrade\">\n" +
                         "    </iframe>\n" +
                         "</body>\n" +
                         "</html>";
@@ -124,7 +137,7 @@ public class restaurents_rtn extends JFrame implements ActionListener {
 
                 // Create a JFXPanel
                 JFXPanel jfxPanel = new JFXPanel();
-                jfxPanel.setSize(1366, 711);
+                jfxPanel.setSize(1366, 771);
 
                 // Set up the JFXPanel with the WebView
                 jfxPanel.setScene(new Scene(webView));
