0642b36ddbefe6817045341d959bfaa250f4cc29
Sutar_here <152772531+carpentrycoder@users.noreply.github.com>
2024-03-19 10:03:18 +0530

    new file
    Added ActionListener functionality to template buttons:
    Integrated ActionListener functionality to the "Ask Me", "Map", "Direction", and "Weather" buttons in the Mum Template.
    Each button now triggers the Delivery_web function from restaurent_func.java with corresponding URLs for delivery services, maps, directions, and weather forecasts.
    Updated URL placeholders with actual links for delivery services and weather forecasts.
    Ensured seamless navigation within the Mum Template application.

---
+++ src/mum_template.java
@@ -0,0 +1,39 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class mum_template{
+    public static void main(String[] args) {
+        PlaceTemplateFunc.PlaceTemplateFunc("Mumbai","Dash_Icons/queens-necklace.png","Dash_Icons/gate-of-india.png","Dash_Icons/famous-mum.png","Dash_Icons/info-mum.png","Dash_Icons/vadapav.png","Dash_Icons/mum-client1.png","Dash_Icons/mum-client2.png","Dash_Icons/mum-client3.png");
+        PlaceTemplateFunc.getaskme().addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String url = "http://surl.li/rrunl"; // Replace this with your actual delivery website URL
+                restaurent_func.Delivery_web(url);
+            }
+        });
+
+        PlaceTemplateFunc.getMapButton().addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String url = "http://surl.li/rrupi"; // Replace this with your actual delivery website URL
+                restaurent_func.Delivery_web(url);
+            }
+        });
+
+        PlaceTemplateFunc.getDirectionButton().addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String url = "http://surl.li/rrunl"; // Replace this with your actual delivery website URL
+                restaurent_func.Delivery_web(url);
+            }
+        });
+
+        PlaceTemplateFunc.getWeatherButton().addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String url = "https://www.accuweather.com/en/in/mumbai/204842/weather-forecast/204842"; // Replace this with your actual delivery website URL
+                restaurent_func.Delivery_web(url);
+            }
+        });
+    }
+}
